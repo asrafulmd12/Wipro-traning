298e4adc6c8426c8e7ddffe21a90761c993a0fae
MdAsraful <MdAsraful@DESKTOP-UJH0C9S>
2026-05-27 23:41:06 +0530

    commit initially

---
+++ Cucumber_BBD/src/test/java/StepDefination/TestRunner.java
@@ -0,0 +1,25 @@
+package StepDefination;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"StepDefination",
+    		"Hooks"},
+    tags = "@Smoke",
+    plugin = {
+            "pretty",
+            "html:target/htmlreports/report.html",
+            "json:target/jsonreport/report.json",
+            "junit:target/junitreport/report.xml"
+    }
+)
+
+public class TestRunner {
+
+}
