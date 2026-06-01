2dc50e6d3800dbc2d2dd101ed61a01b7abb13455
MD ASRAFUL <mahammadasraful66@gmail.com>
2026-06-01 17:48:18 +0530

    first

--- TutorialsNinja/src/test/java/StepDefinition/TutorialsNinjaSteps.java
+++ TutorialsNinja/src/test/java/StepDefinition/TutorialsNinjaSteps.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import org.junit.Assert;
 
 import POM_test.TutorialsNinjapage;
 
