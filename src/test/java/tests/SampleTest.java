9d43556ab14bd585a246eef7d59cbd5f28f61eef
MD ASRAFUL <mahammadasraful66@gmail.com>
2026-06-05 12:44:48 +0530

    Initial commit

---
+++ src/test/java/tests/SampleTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void verifyTest() {
+		System.out.println("TestNG Running");
+		Assert.assertTrue(true);
+	}
+}
