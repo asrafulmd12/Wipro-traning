3e3a6125acc31829834da01d08195a6c16be8bca
MD ASRAFUL <mahammadasraful66@gmail.com>
2026-06-11 13:55:36 +0530

    commit.....

--- Automation_Testing_Practice/src/test/java/utils/ExcelUtils.java
+++ Automation_Testing_Practice/src/test/java/utils/ExcelUtils.java
@@ -1,26 +1,31 @@
 package utils;
 
 import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.*;
 
 public class ExcelUtils {
 
     public static Object[] getSingleRowData(String filePath, String sheetName) {
 
-        Object[] data = new Object[4]; // 4 columns
+        Object[] data = new Object[4];
 
         try {
             FileInputStream fis = new FileInputStream(filePath);
             XSSFWorkbook workbook = new XSSFWorkbook(fis);
             XSSFSheet sheet = workbook.getSheet(sheetName);
 
-            XSSFRow row = sheet.getRow(1); // ONLY FIRST DATA ROW (index 1)
+            XSSFRow row = sheet.getRow(1);
+
+            DataFormatter formatter = new DataFormatter();
 
             for (int i = 0; i < 4; i++) {
-                data[i] = row.getCell(i).toString();
+                data[i] = formatter.formatCellValue(row.getCell(i));
             }
 
             workbook.close();
+            fis.close();
 
         } catch (Exception e) {
             e.printStackTrace();
